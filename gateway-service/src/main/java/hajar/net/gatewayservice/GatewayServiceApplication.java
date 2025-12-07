fd43973aaa4dacda3ba0038d94de79972e5618f1
HARKAOUI Hajar <105939774+hajargithub@users.noreply.github.com>
2025-12-07 12:29:46 +0100

    Configuration statique du système de routage (Gateway avec application.yml OR RouteLocator )

--- gateway-service/src/main/java/hajar/net/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/hajar/net/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,24 @@ package hajar.net.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("r1",predicates -> predicates.path("/customers/*")
+                        .uri("lb://CUSTOMER-SERVICE"))
+                .route("r2",predicates -> predicates.path("/products/*").
+                        uri("lb://INVENTORY-SERVICE"))
+                .build();
     }
 
 }
