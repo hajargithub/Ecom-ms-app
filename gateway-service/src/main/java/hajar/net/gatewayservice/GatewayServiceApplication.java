4928c557f61c78da72ca7f0403a17ef43649babc
HARKAOUI Hajar <105939774+hajargithub@users.noreply.github.com>
2025-12-07 12:49:13 +0100

    configuration dynamique des routes de la gateway

--- gateway-service/src/main/java/hajar/net/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/hajar/net/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package hajar.net.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +15,7 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-    @Bean
+    //@Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("r1",predicates -> predicates.path("/customers/*")
@@ -20,6 +23,11 @@ public class GatewayServiceApplication {
                 .route("r2",predicates -> predicates.path("/products/*").
                         uri("lb://INVENTORY-SERVICE"))
                 .build();
+    }
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator dynamicroutes(
+            ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp ){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 }
